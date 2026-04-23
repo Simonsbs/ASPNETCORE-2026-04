c2ec8e9f9a634af6fd58caa9d2b69653587721db
Simon Stirling <simonsbs@gmail.com>
2026-04-23 10:03:43 +0300

    .

--- Demo1/Controllers/CitiesController.cs
+++ Demo1/Controllers/CitiesController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Demo1.Controllers {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/cities")]
     public class CitiesController : ControllerBase {
 
         [HttpGet]
--- Demo1/Controllers/LandMarksController.cs
+++ Demo1/Controllers/LandMarksController.cs
@@ -2,7 +2,17 @@
 
 namespace Demo1.Controllers {
     [ApiController]
+    [Route("api/cities/{cityID}/landmarks")]
     public class LandMarksController : Controller {
-        
+        [HttpGet]
+        public ActionResult GetLandMarks(int cityID) {
+            var city = DataStores.CitiesDataStore.Current.FirstOrDefault(c => c.ID == cityID);
+
+            if (city == null) {
+                return NotFound();
+            }
+
+            return Ok(city.LandMarks);
+        }
     }
 }
